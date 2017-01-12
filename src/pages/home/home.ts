b4256ead5bfe6cec1377b3938bce8afa9d045dcd
Ted Wei <jdwei@hotmail.com>
2017-01-12 11:04:11 +0800

    email sample

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { EmailComposer } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController) {
     
+  }
+
+  onSendEmailClicked(): void {
+    console.log("onSendEmailClicked()");
+
+    if (!this.platform.is('cordova')) {
+      let alert = this.alertCtrl.create({
+        title: 'Error!',
+        subTitle: 'Not a Cordova platform!',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+
+    EmailComposer.isAvailable().then((available: boolean) =>{
+      console.log("available: " + available); // It seems 'available' is undefined
+      // if(!available) {
+        let email = {
+          to: 'jdwei@hotmail.com',
+          // cc: 'erika@mustermann.de',
+          // bcc: ['john@doe.com', 'jane@doe.com'],
+          // attachments: [
+          //   'file://img/logo.png',
+          //   'res://icon.png',
+          //   'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+          //   'file://README.pdf'
+          // ],
+          subject: 'Cordova Icons',
+          body: 'How are you? Nice greetings from Ted',
+          isHtml: true
+        };
+        // Send a text message using default options
+        EmailComposer.open(email);
+        console.log("open email app");
+      // } else {
+      //   let alert = this.alertCtrl.create({
+      //     title: 'Error!',
+      //     subTitle: 'email is not available!',
+      //     buttons: ['OK']
+      //   });
+      //   alert.present();
+      // }
+    });
   }
 
 }
